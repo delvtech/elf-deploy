e413971229bbfa13f9744b6dd999a27108492e34
jonny rhea <jonathan.rhea@gmail.com>
2020-10-20 11:43:31 -0500

    add README.md and added printouts to deploy-contracts.sh

--- deploy-contracts.sh
+++ deploy-contracts.sh
@@ -2,6 +2,7 @@
 
 source elf.env
 
+echo "Download contracts..."
 # link/clone and build contracts
 if [ $1="local" ]; then
     ln -sf ../../elf-contracts . 
@@ -9,7 +10,8 @@ else
     git clone git@github.com:element-fi/elf-contracts.git 
 fi
 
-cd elf-contracts && make init && make build 
+echo "Build contracts..."
+cd elf-contracts && make init && make build
 cd ../
 ln -sf elf-contracts/out .
 ln -sf elf-contracts/scripts/deploy.sh .
