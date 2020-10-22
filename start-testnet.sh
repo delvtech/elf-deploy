72ca5c36d141e875520300475c821d568ce37bf3
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2020-10-22 13:03:29 -0500

    mods made to localnet to allow account with password (#3)

--- start-testnet.sh
+++ start-testnet.sh
@@ -2,9 +2,12 @@
 
 source elf.env
 
+rm -rf $HOME/.dapp/testnet
 mkdir -p $HOME/.dapp/testnet/snapshots
 
+
 # copy localnet config to snapshots folder
+unlink -f $HOME/.dapp/testnet/snapshots/localnet
 ln -sf $LOCALNET_DIR $HOME/.dapp/testnet/snapshots/
 
 # launch network
