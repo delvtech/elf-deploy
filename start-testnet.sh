ec4e7a8eef58cb3dade53f32cac58d34eb3c9a48
jonny rhea <jonathan.rhea@gmail.com>
2020-10-22 18:00:29 -0500

    replace unlink with rm also make sure keystore dir exists

--- start-testnet.sh
+++ start-testnet.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.dapp/testnet/snapshots
 
 
 # copy localnet config to snapshots folder
-unlink -f $HOME/.dapp/testnet/snapshots/localnet
+rm -rf $HOME/.dapp/testnet/snapshots/localnet
 ln -sf $LOCALNET_DIR $HOME/.dapp/testnet/snapshots/
 
 # launch network
