bab37ccc828344b2684a4d3b96d07bab643c8f74
Matthew Brown <mattfdbrown@gmail.com>
2020-11-03 08:58:38 -0800

    remove verbose

--- start-testnet.sh
+++ start-testnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd localnet
 # launch network
-ganache-cli  --account_keys_path="./keystore/keys.json" --mnemonic="foo" --gasLimit=9007199254740991 --allowUnlimitedContractSize --defaultBalanceEther=100 --accounts=1 --chainId=99 --verbose
+ganache-cli  --account_keys_path="./keystore/keys.json" --mnemonic="foo" --gasLimit=9007199254740991 --allowUnlimitedContractSize --defaultBalanceEther=100 --accounts=1 --chainId=99 
