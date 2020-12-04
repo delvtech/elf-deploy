798665c9e366165ca233eb2cd81ccf9c32f93e70
Danny Delott <dannydelott@gmail.com>
2020-12-03 16:29:11 -0800

    Add script to copy contracts.json into frontend

---
+++ copy-contracts-into-frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Copying contracts.json from elf-contracts into frontend"
+pwd
+CONTRACTS_JSON_SOURCE=./localnet/out/contracts.json
+CONTRACTS_JSON_DESTINATION=./frontend/efi-frontend/frontend/src/contracts.json
+
+rm -f "$CONTRACTS_JSON_DESTINATION"
+cp "$CONTRACTS_JSON_SOURCE" "$CONTRACTS_JSON_DESTINATION"
