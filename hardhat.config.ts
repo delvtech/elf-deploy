851ac3c2f4202736c92b41f630ca1223450cb74c
Matthew Brown <mattfdbrown@gmail.com>
2021-01-30 13:19:37 -0800

    add comment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "hardhat-typechain";
 
 import { HardhatUserConfig, task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+// hre is provided on the global scope.  We cannot import it here or in any file we import since we
+// cannot load the hre while doing configuration.
 declare global {
   var hre: HardhatRuntimeEnvironment;
 }
