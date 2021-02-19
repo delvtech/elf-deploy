3d71c7be2c325b36e9446134f0611aadc71e856e
Danny Delott <dannydelott@gmail.com>
2021-02-18 20:46:55 -0800

    Makes tranche a weeklong (#14)

--- src/scripts/tranche.ts
+++ src/scripts/tranche.ts
@@ -3,11 +3,13 @@ import { Elf, Tranche } from "types";
 
 import { Tranche__factory } from "../types";
 
+const ONE_DAY_IN_SECONDS = 86400;
+const ONE_WEEK_IN_SECONDS = 7 * ONE_DAY_IN_SECONDS;
 export async function deployTranche(elfContract: Elf, signer: Signer) {
   const TrancheDeployer = new Tranche__factory(signer);
   const trancheContract = await TrancheDeployer.deploy(
     elfContract.address,
-    86400 // time length of tranche in seconds
+    ONE_WEEK_IN_SECONDS
   );
 
   return trancheContract;
