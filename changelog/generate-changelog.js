0bc1bfd6e356a59d22e48bdce568c720ff83b96d
jonny rhea <jonathan.rhea@gmail.com>
2021-05-11 17:44:31 -0500

    change from table to unordered list

--- changelog/generate-changelog.js
+++ changelog/generate-changelog.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const path = require("path");
 const json2md = require("json2md")
 
+let json = [
+    { h1: "Element Finance Changelog" },
+]
 
 let files  = [];
 
@@ -29,7 +32,7 @@ files.forEach(file => {
     networks.push(info[1])
 });
 networks = Array.from(new Set(networks));
-console.log(networks);
+//console.log(networks);
 
 // get different releases in each network
 // end result is something like: [["v1.0.0-a.1", "v1.0.0"],["v1.0.0-a.2", "v1.0.1"]]
@@ -38,6 +41,7 @@ let index = -1;
 networks.forEach(network => {
     releases.push([]);
     index += 1;
+    json.push({ h2: network})
     files.forEach(file => {
         // info looks something like: 'releases/goerli/v1.0.0-a.1/addresses.json' 
         let info = file.split('/');
@@ -46,17 +50,16 @@ networks.forEach(network => {
             let url = "https://raw.githubusercontent.com/element-fi/elf-deploy/release-management/changelog/releases/" 
             // info[2] is v1.0.0-a.1
             url = url + network + "/" + info[2] + "/addresses.json"
-            releases[index].push({ title: info[2], source: url })
+            releases[index].push({ link: { title: info[2], source: url } })
         }
     })
+    json.push({ ul: releases[index] })
 });
-// console.log(releases);
+console.log(json);
 
 
-let json = [
-    { h1: "Element Finance Changelog" },
-    { table: { headers: networks, rows: releases  } }
-]
+
+
 let markdown = json2md(json)
 
 
