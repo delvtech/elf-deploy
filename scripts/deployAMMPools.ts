579aebed6e1577bd711f934a5cde70b266850bdb
Paul Vienhage <pevienhage@gmail.com>
2021-06-23 19:21:35 +0200

    Added Etherscan Verification to Interest Tokens (#37)
    * added verification to interest tokens
    
    * get rid of colon

--- scripts/deployAMMPools.ts
+++ scripts/deployAMMPools.ts
@@ -106,7 +106,7 @@ export async function deployConvergentPool(
       tParam*ONE_YEAR_IN_SECONDS,
       ethers.utils.parseEther(swapFee),
       `LP ${assetName}`,
-      `LP:${assetSymbol}`,
+      `Lp${assetSymbol}`,
       {
         gasPrice: ethers.utils.parseUnits(gas, 'gwei')
       }
@@ -144,7 +144,7 @@ export async function deployConvergentPool(
             feeGov,
             gov,
             `LP ${assetName}`,
-            `LP:${assetSymbol}`,
+            `Lp${assetSymbol}`,
         ],
     });
   
@@ -207,9 +207,9 @@ async function deployWithAddresses(addresses: any, network: string) {
         }
 
         const ytName = await yt.name();
-        const lpTokenName = `LP ${ytName}`;
+        const lpTokenName = `Lp ${ytName}`;
         const ytSymbol = await yt.name();
-        const lpTokenSymbol = `LP:${ytSymbol}`;
+        const lpTokenSymbol = `Lp${ytSymbol}`;
 
         let tokens;
         let weights;
