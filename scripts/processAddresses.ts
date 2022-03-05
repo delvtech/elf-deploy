2a1ef7ca3920d43e8fa8fd5734e64e8cf45df9ac
Matthew Brown <mattfdbrown@gmail.com>
2022-03-04 17:17:19 -0800

    refactor processAddresses

--- scripts/processAddresses.ts
+++ scripts/processAddresses.ts
@@ -3,85 +3,129 @@ import * as fs from "fs";
 import * as hre from "hardhat";
 import * as readline from "readline-sync";
 
+import { AddressesJsonFile } from "../addresses/AddressesJsonFile.d";
+import goerliJson from "../addresses/goerli.json";
+import mainnetJson from "../addresses/mainnet.json";
+
+type GoerliBaseAssets = keyof typeof goerliJson["tranches"];
+type MainnetBaseAssets = keyof typeof mainnetJson["tranches"];
+
+interface Tranche {
+  address: string;
+  convergentCurvePoolFactory: string;
+  expiration: number;
+  ptPool: {
+    address: string;
+    fee: string;
+    poolId: string;
+    timeStretch: number;
+  };
+  trancheFactory: string;
+  weightedPoolFactory: string;
+  ytPool: {
+    address: string;
+    fee: string;
+    poolId: string;
+  };
+}
 
 async function main() {
-    // get the network name
-    let network = hre.network.name == "hardhat"? "mainnet": hre.network.name;
+  // get the network name
+  const network = hre.network.name == "hardhat" ? "mainnet" : hre.network.name;
 
-    // read in address file and parse
-    let rawdata = fs.readFileSync("addresses/"+network+".json").toString();
-    let addressesFile = JSON.parse(rawdata);
+  // get addresses for safelist
+  let safeList: string[] = [];
+  if (network === "mainnet") {
+    safeList = getSafeList(mainnetJson.tranches);
+  }
+  if (network === "goerli") {
+    safeList = getSafeList(goerliJson.tranches);
+  }
 
-    let safeList = [];
-    let baseAssets = [];
-    // get addresses for safelist
-    for (const trancheListKey in addressesFile["tranches"]) {
-        baseAssets.push(trancheListKey);
-        const trancheList = addressesFile["tranches"][trancheListKey];
-        for (const tranche of trancheList) {
-            safeList.push(utils.getAddress(tranche.address))
-            safeList.push(utils.getAddress(tranche.ptPool.address))
-            safeList.push(utils.getAddress(tranche.ytPool.address))
-        }
-    }
+  let chainid = providers.getNetwork(network).chainId;
 
-    let chainid = providers.getNetwork(network).chainId;
+  // read in address file and parse
+  const addressesFile = network === "mainnet" ? mainnetJson : goerliJson;
 
-    let addresses: any = {}
-    // add the rest of the known address types
-    addresses = {
-        balancerVaultAddress: utils.getAddress(addressesFile["balancerVault"]),
-        convergentPoolFactoryAddress: utils.getAddress(addressesFile["convergentCurvePoolFactory"]),
-        trancheFactoryAddress: utils.getAddress(addressesFile["trancheFactory"]),
-        userProxyContractAddress: utils.getAddress(addressesFile["userProxy"]),
-        weightedPoolFactoryAddress: utils.getAddress(addressesFile["weightedPoolFactory"]),
-        deprecatedCCPoolFactoryAddresses: addressesFile["deprecatedCCPoolFactory"]
-    }
+  // add the rest of the known address types
+  const balancerVaultAddress = utils.getAddress(addressesFile["balancerVault"]);
+  const convergentPoolFactoryAddress = {
+    v1_1: utils.getAddress(addressesFile.convergentCurvePoolFactory.v1_1),
+    v1: utils.getAddress(addressesFile.convergentCurvePoolFactory.v1),
+  };
+  const trancheFactoryAddress = utils.getAddress(
+    addressesFile["trancheFactory"]
+  );
+  const userProxyContractAddress = utils.getAddress(addressesFile["userProxy"]);
+  const weightedPoolFactoryAddress = utils.getAddress(
+    addressesFile["weightedPoolFactory"]
+  );
 
-    // add base asset tokens
-    for (const baseAsset of baseAssets) {
-        const keyName = baseAsset+"Address"
-        addresses[keyName]=utils.getAddress(addressesFile["tokens"][baseAsset])
-    }
+  const addresses: AddressesJsonFile["addresses"] = {
+    balancerVaultAddress,
+    trancheFactoryAddress,
+    weightedPoolFactoryAddress,
+    convergentPoolFactoryAddress,
+    userProxyContractAddress,
+    wbtcAddress: getBaseAssetAddress(addressesFile.tokens, "wbtc"),
+    wethAddress: getBaseAssetAddress(addressesFile.tokens, "weth"),
+    daiAddress: getBaseAssetAddress(addressesFile.tokens, "dai"),
+    usdcAddress: getBaseAssetAddress(addressesFile.tokens, "usdc"),
+    eurscrvAddress: getBaseAssetAddress(addressesFile.tokens, "eurscrv"),
+    stecrvAddress: getBaseAssetAddress(addressesFile.tokens, "stecrv"),
+    crv3cryptoAddress: getBaseAssetAddress(addressesFile.tokens, "crv3crypto"),
+    crvtricryptoAddress: getBaseAssetAddress(
+      addressesFile.tokens,
+      "crvtricrypto"
+    ),
+    "lusd3crv-fAddress": getBaseAssetAddress(
+      addressesFile.tokens,
+      "lusd3crv-f"
+    ),
+    "alusd3crv-fAddress": getBaseAssetAddress(
+      addressesFile.tokens,
+      "alusd3crv-f"
+    ),
+    "mim-3lp3crv-fAddress": getBaseAssetAddress(
+      addressesFile.tokens,
+      "mim-3lp3crv-f"
+    ),
+  };
 
-    // Add base assets that exist in the network that was NOT selected to the json
+  // frontend json structure
+  const frontendAddressesJsonFile: AddressesJsonFile = {
+    addresses: addresses,
+    chainId: chainid,
+    safelist: safeList,
+  };
 
-    // get NOT the network name
-    let notNetwork = network == "mainnet"? "goerli": "mainnet";
-    // read in address file and parse
-    let notNetworkRawdata = fs.readFileSync("addresses/"+notNetwork+".json").toString();
-    let notNetworkAddressesFile = JSON.parse(notNetworkRawdata);
-    let notNetworkBaseAssets = [];
-    // store base assets listed in NOT network json
-    for (const trancheListKey in notNetworkAddressesFile["tranches"]) {
-        notNetworkBaseAssets.push(trancheListKey);
-    }
+  const formattedAddressesJsonFile = JSON.stringify(
+    frontendAddressesJsonFile,
+    null,
+    4
+  );
 
-    // add base asset tokens that are NOT already added
-    for (const notNetworkBaseAsset of notNetworkBaseAssets) {
-        const keyName = notNetworkBaseAsset+"Address"
-        if (!addresses.hasOwnProperty(keyName)){
-            addresses[keyName]="0x0000000000000000000000000000000000000000";
-        }
-    }
+  console.log(formattedAddressesJsonFile);
 
-    // frontend json structure
-    let frontend = {
-        addresses: addresses,
-        chainId: chainid,
-        safelist: safeList
-    };
+  fs.writeFileSync(
+    "addresses/frontend-" + network + ".addresses.json",
+    formattedAddressesJsonFile,
+    "utf8"
+  );
 
-    let frontendJson = JSON.stringify(frontend, null, 4);
-    console.log(frontendJson);
-    fs.writeFileSync('addresses/frontend-'+network+'.addresses.json', frontendJson,'utf8');
-
-    if(process.env["WRITE_CHANGELOG"]=="1"){
-        // get release version
-        const releaseVersion = readline.question("Release Version (e.g. vX.X.X:X): ");
-        fs.mkdirSync("changelog/releases/"+network+"/"+releaseVersion, { recursive: true })
-        fs.copyFileSync("addresses/"+network+".json","changelog/releases/"+network+"/"+releaseVersion+"/addresses.json")
-    }
+  if (process.env["WRITE_CHANGELOG"] == "1") {
+    // get release version
+    const releaseVersion = readline.question(
+      "Release Version (e.g. vX.X.X:X): "
+    );
+    fs.mkdirSync("changelog/releases/" + network + "/" + releaseVersion, {
+      recursive: true,
+    });
+    fs.copyFileSync(
+      "addresses/" + network + ".json",
+      "changelog/releases/" + network + "/" + releaseVersion + "/addresses.json"
+    );
+  }
 }
 
 main()
@@ -90,3 +134,31 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+function getSafeList(tranchesByBaseAsset: Record<string, Tranche[]>): string[] {
+  const safeList: string[] = [];
+  Object.values(tranchesByBaseAsset).forEach((tranches) => {
+    tranches.forEach((tranche) => {
+      safeList.push(utils.getAddress(tranche.address));
+      safeList.push(utils.getAddress(tranche.ptPool.address));
+      if (tranche.ytPool) {
+        safeList.push(utils.getAddress(tranche.ytPool.address));
+      }
+    });
+  });
+
+  return safeList;
+}
+
+function getBaseAssetAddress(
+  tokens: Record<string, string>,
+  baseAsset: GoerliBaseAssets | MainnetBaseAssets
+) {
+  const asset = tokens[baseAsset];
+
+  if (asset) {
+    return utils.getAddress(asset);
+  }
+
+  return "0x0000000000000000000000000000000000000000";
+}
