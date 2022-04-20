f69a09a88b457678a829800778630664d72b13d5
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-04-20 17:36:54 +0100

    dynamic set block

--- scripts/runSetZapSwapCurveApprovals.ts
+++ scripts/runSetZapSwapCurveApprovals.ts
@@ -2,17 +2,24 @@ import { ethers } from "hardhat";
 import mainnet from "../addresses/mainnet.json";
 import { impersonate } from "./impersonate";
 import { setBlock } from "./setBlock";
+import { ethers as eeethers } from "ethers";
+import config from "../hardhat.config";
 import { setZapSwapCurveApprovals } from "./setZapSwapCurveApprovals";
 
 const zapSwapCurveOwner = "0x422494292e7a9Dda8778Bb4EA05C2779a3d60f5D";
 
+const mainnetProvider = new eeethers.providers.JsonRpcProvider(
+  config.networks?.hardhat?.forking?.url
+);
+
 async function main() {
   let [signer] = await ethers.getSigners();
 
+  const latestBlock = await mainnetProvider.getBlockNumber();
   const network = await signer.provider?.getNetwork();
   switch (network?.chainId) {
     case 31337: {
-      await setBlock(14617007); // Mar-26-2022 01:40:40 AM
+      await setBlock(latestBlock - 1000);
       signer = await impersonate(zapSwapCurveOwner);
     }
     case 1: {
